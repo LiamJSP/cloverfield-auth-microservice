28a4064a7dc57333bd70f770791313218b1447fa
Liam Sherman Parris <lshermanparris@protonmail.com>
2023-06-22 16:05:13 -0300

    Refactoring lambda handlers to have more self-contained context

--- src/handlers/authHandlers.ts
+++ src/handlers/authHandlers.ts
@@ -11,7 +11,7 @@ import * as jwt from "jsonwebtoken";
 import authy from "authy";
 import { User } from "../entities/User";
 import { ActiveSession } from "../entities/ActiveSession";
-import crypto from 'crypto';
+import crypto from "crypto";
 
 // Function to generate a default key with AES for JWT token generation
 // This is used if a key is not manually set
@@ -21,51 +21,33 @@ function generateDefaultKeyWithAES(): Buffer {
   return key;
 }
 
-// Configuration variables for JWT, token expiration time, and Authy
-const JWT_SECRET: string = process.env.JWT_SECRET || generateDefaultKeyWithAES().toString('base64');
-const TOKEN_EXPIRATION_TIME:string = process.env.TOKEN_EXPIRATION_TIME || "1h";
-const AUTHY_API_KEY:string = process.env.AUTHY_API_KEY || "no_key_in_env";
-const authyClient = authy(AUTHY_API_KEY);
-
-const AppDataSource = new DataSource({
-  type: "postgres",
-  host: process.env.DB_HOST,
-  port: Number(process.env.DB_PORT) || 5432,
-  username: process.env.DB_USERNAME,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  entities: [`${__dirname}/entities/*{.ts,.js}`],
-  synchronize: true,
-});
-
-let initializedAppDataSource: DataSource;
-let databaseConnectionTimer: NodeJS.Timeout;
-
-async function initialize(): Promise<DataSource> {
-  if (!initializedAppDataSource) {
-    databaseConnectionTimer = setTimeout(() => {
-      console.error("Database connection timeout after 6 seconds.");
-      clearTimeout(databaseConnectionTimer);
-      throw new Error("Database connection timeout");
+async function initialize(AppDataSource: DataSource): Promise<DataSource> {
+  let initializedAppDataSource: DataSource;
+  const dbInitPromise = AppDataSource.initialize();
+  const timeoutPromise = new Promise((_, reject) => {
+    const id = setTimeout(() => {
+      clearTimeout(id);
+      reject(new Error("Database connection timeout after 6 seconds"));
     }, 6000);
+  });
 
-    try {
-      await AppDataSource.initialize();
-      console.log("Data Source has been initialized!");
-      initializedAppDataSource = AppDataSource;
-      clearTimeout(databaseConnectionTimer);
-    } catch (error) {
-      console.error("Database connection error:", error);
-      clearTimeout(databaseConnectionTimer);
-      throw new Error("Database connection failed");
-    }
+  try {
+    await Promise.race([dbInitPromise, timeoutPromise]);
+    console.log("Data Source has been initialized!");
+    initializedAppDataSource = AppDataSource;
+  } catch (error) {
+    console.error("Database connection error:", error);
+    throw error;
   }
   return initializedAppDataSource;
 }
 
-async function safeInitialize(): Promise<APIGatewayProxyResult | null> {
+async function safeInitialize(
+  AppDataSource: DataSource
+): Promise<APIGatewayProxyResult | DataSource> {
   try {
-    await initialize();
+    const initializedDataSource = await initialize(AppDataSource);
+    return initializedDataSource;
   } catch (error) {
     let errorMessage = "An unknown error occurred.";
     if (error instanceof Error) {
@@ -74,10 +56,7 @@ async function safeInitialize(): Promise<APIGatewayProxyResult | null> {
     }
     return createApiResponse(500, { error: errorMessage });
   }
-  return null;
 }
-
-
 
 // Helper function to create an API Gateway response
 export function createApiResponse(
@@ -90,6 +69,10 @@ export function createApiResponse(
   };
 }
 
+function isApiGatewayResponse(object: any): object is APIGatewayProxyResult {
+  return "statusCode" in object;
+}
+
 
 // Function to register a new user
 export async function register(
@@ -97,10 +80,30 @@ export async function register(
 ): Promise<APIGatewayProxyResult> {
   console.log("Register fired, pre DB");
 
-  const initializationError = await safeInitialize();
-  if (initializationError) return initializationError;
+  const AppDataSource = new DataSource({
+    type: "postgres",
+    host: process.env.DB_HOST,
+    port: Number(process.env.DB_PORT) || 5432,
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    entities: [`${__dirname}/entities/*{.ts,.js}`],
+    synchronize: true,
+  });
+
+  const initializedDataSourceOrError = await safeInitialize(AppDataSource);
+  if (isApiGatewayResponse(initializedDataSourceOrError)) {
+    // This is an error
+    return initializedDataSourceOrError;
+  }
 
   console.log("DB initialized successfully.");
+  const JWT_SECRET: string = process.env.JWT_SECRET || generateDefaultKeyWithAES().toString("base64");
+  const TOKEN_EXPIRATION_TIME: string = process.env.TOKEN_EXPIRATION_TIME || "1h";
+  const AUTHY_API_KEY: string = process.env.AUTHY_API_KEY || "no_key_in_env";
+  const authyClient = authy(AUTHY_API_KEY);
+  
+
   // Parses the request body
   const body = JSON.parse(event.body || "{}");
   const { username, password, email, phone_number, country_code } = body;
@@ -153,8 +156,12 @@ export async function register(
               // Log successful user save
               console.log(`Successfully saved user: ${username}`);
             } catch (error) {
-              console.error('Error saving user to database:', error);
-              resolve(createApiResponse(500, { error: 'Error saving user to database' }));
+              console.error("Error saving user to database:", error);
+              resolve(
+                createApiResponse(500, {
+                  error: "Error saving user to database",
+                })
+              );
               return;
             }
 
@@ -189,17 +196,35 @@ export async function register(
   return registerUserResponse;
 }
 
-
-
 export async function login(
   event: APIGatewayProxyEvent
 ): Promise<APIGatewayProxyResult> {
   console.log("Register fired, pre DB");
 
-  const initializationError = await safeInitialize();
-  if (initializationError) return initializationError;
+  const AppDataSource = new DataSource({
+    type: "postgres",
+    host: process.env.DB_HOST,
+    port: Number(process.env.DB_PORT) || 5432,
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    entities: [`${__dirname}/entities/*{.ts,.js}`],
+    synchronize: true,
+  });
+
+  const initializedDataSourceOrError = await safeInitialize(AppDataSource);
+  if (isApiGatewayResponse(initializedDataSourceOrError)) {
+    // This is an error
+    return initializedDataSourceOrError;
+  }
 
   console.log("DB initialized successfully.");
+
+  const JWT_SECRET: string = process.env.JWT_SECRET || generateDefaultKeyWithAES().toString("base64");
+  const TOKEN_EXPIRATION_TIME: string = process.env.TOKEN_EXPIRATION_TIME || "1h";
+  const AUTHY_API_KEY: string = process.env.AUTHY_API_KEY || "no_key_in_env";
+  const authyClient = authy(AUTHY_API_KEY);
+
   // Parses the request body
   const body = JSON.parse(event.body || "{}");
   const { username, password, authy_code } = body;
@@ -259,17 +284,36 @@ export async function login(
   return createApiResponse(200, { token });
 }
 
-
 // Function to check if a JWT token is valid
 export async function isTokenValid(
   event: APIGatewayProxyEvent
 ): Promise<APIGatewayProxyResult> {
   console.log("Register fired, pre DB");
 
-  const initializationError = await safeInitialize();
-  if (initializationError) return initializationError;
+  const AppDataSource = new DataSource({
+    type: "postgres",
+    host: process.env.DB_HOST,
+    port: Number(process.env.DB_PORT) || 5432,
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    entities: [`${__dirname}/entities/*{.ts,.js}`],
+    synchronize: true,
+  });
+
+  const initializedDataSourceOrError = await safeInitialize(AppDataSource);
+  if (isApiGatewayResponse(initializedDataSourceOrError)) {
+    // This is an error
+    return initializedDataSourceOrError;
+  }
 
   console.log("DB initialized successfully.");
+
+  const JWT_SECRET: string = process.env.JWT_SECRET || generateDefaultKeyWithAES().toString("base64");
+  const TOKEN_EXPIRATION_TIME: string = process.env.TOKEN_EXPIRATION_TIME || "1h";
+  const AUTHY_API_KEY: string = process.env.AUTHY_API_KEY || "no_key_in_env";
+  const authyClient = authy(AUTHY_API_KEY);
+
   // Retrieves the token from the authorization header
   const token = event.headers.authorization?.split(" ")[1];
 
