e991d6b08be68294024e73dd2ee0a0954803db16
Liam Sherman Parris <lshermanparris@protonmail.com>
2023-07-08 21:57:47 -0300

    Adding more verbose logging to isValidToken

--- src/handlers/authHandlers.ts
+++ src/handlers/authHandlers.ts
@@ -317,7 +317,7 @@ export async function isTokenValid(event: APIGatewayProxyEvent): Promise<APIGate
   const JWT_SECRET: string = process.env.JWT_SECRET || generateDefaultKeyWithAES().toString("base64");
 
   const token = event.headers.authorization?.split(" ")[1];
-
+  console.log("token: " + token + " event header: " + event.headers.authorization);
   if (!token) {
     return createApiResponse(401, { error: "No token provided" });
   }
